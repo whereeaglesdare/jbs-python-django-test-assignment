95d063724147b922073cac0a1dfd37c32546f640
whereeaglesdare <belokur.bogdan@yandex.ru>
2018-02-07 20:34:08 +0200

    Login page

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='user_info'),
+    url(r'^login/$', auth_views.login, {'template_name': 'user_profile/login.html'}, name='login'),
 ]
