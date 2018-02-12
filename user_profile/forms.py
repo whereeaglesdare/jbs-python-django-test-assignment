5cf90312a971f4750f5c215e4e764da33434c79e
whereeaglesdare <belokur.bogdan@yandex.ru>
2018-02-12 02:06:24 +0200

    Widget customization

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 
 class MyDateInput(forms.DateInput):
-    input_type =  'date'
-    attrs = {'class': 'hasDatepicker'}
+    input_type =  'text'
+    attrs = {'class': 'form-control'}
+    format = ('%m/%d/%Y',)
 
 
 class UserForm(forms.ModelForm):
@@ -19,5 +20,5 @@ class ProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ('bio', 'contacts', 'birth_date', 'profile_photo')
         widgets = {
-            'birth_date': MyDateInput(),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control'}),
         }
