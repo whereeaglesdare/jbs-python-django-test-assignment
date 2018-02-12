5e3aa69ddb23103a6dbebb797cefbce83c6ec8dc
whereeaglesdare <belokur.bogdan@yandex.ru>
2018-02-12 12:11:55 +0200

    Update custom calendar widget. Using bootstrap datepicker as a calendar widget

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 
 class MyDateInput(forms.DateInput):
-    input_type =  'text'
-    attrs = {'class': 'form-control'}
-    format = ('%m/%d/%Y',)
+
+
+    class Media:
+        extend = False
+        css = {'all': ( 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.css',) }
+        js = ( 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.js',)
 
 
 class UserForm(forms.ModelForm):
@@ -20,5 +23,5 @@ class ProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ('bio', 'contacts', 'birth_date', 'profile_photo')
         widgets = {
-            'birth_date': forms.DateInput(attrs={'class': 'form-control'}),
+            'birth_date': MyDateInput(attrs={'class': 'form-control datepicker', "data-toggle": "datepicker"}, format='%d-%m-%Y')
         }
